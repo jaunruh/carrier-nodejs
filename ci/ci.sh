f43ef4f8da07873bc56770c1a9f3c78d75ca2363
Arvid E. Picciani <aep@exys.org>
2019-10-14 19:10:45 +0200

    osx ci maybe
    [publish binary]

---
+++ ci/ci.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -ex
+
+if [ "${TRAVIS_OS_NAME}" = "osx" ];
+then
+  brew update
+  brew install yarn
+fi
+
+npm test
+
+COMMIT_MESSAGE=$(git log --format=%B --no-merges -n 1 | tr -d '\n')
+if [[ "${COMMIT_MESSAGE}" =~ "[publish binary]" ]];
+then
+    yarn upload-binary
+fi;
